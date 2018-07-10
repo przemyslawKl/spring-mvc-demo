b74a91ab3bd4f88567c0e5c9e7e946363849af80
Przemek Klobus <klobus.p@g,ail.com>
2018-07-10 18:27:18 +0200

    1

--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -37,6 +37,13 @@ public class CustomerController {
 	public String processForm(
 			@Valid @ModelAttribute ("customer") Customer theCustomer,
 			BindingResult theBindingResult) {
+		
+		System.out.println("Last name : |" + theCustomer.getLastName() + "|");
+		
+		System.out.println("Binding result : " + theBindingResult);
+		
+		System.out.println("\n\n\n\n");
+
 				
 				if(theBindingResult.hasErrors()) {
 					return "customer-form";
